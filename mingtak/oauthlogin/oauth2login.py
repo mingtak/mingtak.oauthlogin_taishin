20b54f1b7336cd8773fbc8cfb56c8bf7aa2e8256
andy fang <andy@mingtak.com.tw>
2014-09-24 10:20:12 +0100

    modify, facebook login OK, google login fail.

--- mingtak/oauthlogin/oauth2login.py
+++ mingtak/oauthlogin/oauth2login.py
@@ -22,17 +22,69 @@ from mingtak.oauthlogin import MessageFactory as _
 
 # Interface class; used to define content-type schema.
 
+allowWebsite = SimpleVocabulary(
+    [SimpleTerm(value=u'facebook', title=_(u'facebook')),
+     SimpleTerm(value=u'google', title=_(u'google')),]
+)
+"""
+     SimpleTerm(value=u'linkedin', title=_(u'linkedin')),
+     SimpleTerm(value=u'twitter', title=_(u'twitter'))]
+)
+"""
+
+
 class IOauth2Setting(form.Schema, IImageScaleTraversable):
     """
     Oauth2 login method
     """
-    testField = schema.TextLine(
-        title=_(u"Test Field"),
+    allowList = schema.List(
+        title=_(u"Allow List"),
+        description=_(u"Allow list that select website to login."),
+        value_type=schema.Choice(
+                       title=_(u"Allow website"),
+                       vocabulary=allowWebsite,
+                       required=False,
+                   ),
         required=False,
     )
 
-    textField = schema.Text(
-        title=_(u"Text Field"),
+    facebookAppId = schema.TextLine(
+        title=_(u"Facebook app id"),
+        required=False,
+    )
+
+    facebookAppSecret = schema.TextLine(
+        title=_(u"Facebook app secret"),
+        required=False,
+    )
+
+    facebookScope = schema.TextLine(
+        title=_(u"Facebook auth scope"),
+        required=False,
+    )
+
+    facebookRedirectUri = schema.URI(
+        title=_(u"Facebook redirect URL"),
+        required=False,
+    )
+
+    googleAppId = schema.TextLine(
+        title=_(u"Google app id"),
+        required=False,
+    )
+
+    googleAppSecret = schema.TextLine(
+        title=_(u"Google app secret"),
+        required=False,
+    )
+
+    googleScope = schema.TextLine(
+        title=_(u"Google auth scope"),
+        required=False,
+    )
+
+    googleRedirectUri = schema.URI(
+        title=_(u"Google redirect URL"),
         required=False,
     )
 
