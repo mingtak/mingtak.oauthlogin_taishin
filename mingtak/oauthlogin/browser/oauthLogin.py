739b7719a6a3bca955b8d176d2ae708e542524c0
andy fang <andy@mingtak.com.tw>
2014-09-24 17:07:07 +0100

    modify, add class OauthWorkFlow

--- mingtak/oauthlogin/browser/oauthLogin.py
+++ mingtak/oauthlogin/browser/oauthLogin.py
@@ -10,24 +10,45 @@ from Products.CMFPlone.utils import safe_unicode
 from oauthlib.oauth2 import TokenExpiredError
 import os; os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+
 logger = logging.getLogger("mingtak.oauthlogin.browser.oauthLogin")
 
+class OauthWorkFlow(object):
+    prefixString = "mingtak.oauthlogin.oauth2login.IOauth2Setting."
+    oauthServerName = ""
 
-#class OauthWorkFlow(object):
+    def __init__(self, oauthServerName):
+        self.oauthServerName = oauthServerName
 
+    def getRegistryValue(self):
+        prefixString, oauthServerName = self.prefixString, self.oauthServerName
+        registry = getUtility(IRegistry)
+        client_id = registry.get("%s%s%s" % (prefixString, oauthServerName, "AppId"))
+        client_secret = registry.get("%s%s%s" % (prefixString, oauthServerName, "AppSecret"))
+        scope = registry.get("%s%s%s" % (prefixString, oauthServerName, "Scope"))
+        redirect_uri = registry.get("%s%s%s" % (prefixString, oauthServerName, "RedirectUri"))
+        return client_id, client_secret, scope, redirect_uri
 
+    def getUserInfo(self, oauth2Session, token_url, client_secret, code, getUrl):
+        oauth2Session.fetch_token(token_url=token_url,
+                                  client_secret=client_secret,
+                                  code=code)
+        getUser = oauth2Session.get(getUrl)
+        return getUser
+
+    def createUser(self, userid, email, properties):
+        api.user.create(username=userid, email=email, properties=properties,)
+        return
 
 
 class FacebookLogin(BrowserView):
-    token_url = 'https://graph.facebook.com/oauth/access_token'
-    authorization_base_url = 'https://www.facebook.com/dialog/oauth'
+    token_url = "https://graph.facebook.com/oauth/access_token"
+    authorization_base_url = "https://www.facebook.com/dialog/oauth"
+    getUrl = "https://graph.facebook.com/me?"
 
     def __call__(self):
-        registry = getUtility(IRegistry)
-        client_id = registry.get('mingtak.oauthlogin.oauth2login.IOauth2Setting.facebookAppId')
-        client_secret = registry.get('mingtak.oauthlogin.oauth2login.IOauth2Setting.facebookAppSecret')
-        scope = registry.get('mingtak.oauthlogin.oauth2login.IOauth2Setting.facebookScope')
-        redirect_uri = registry.get('mingtak.oauthlogin.oauth2login.IOauth2Setting.facebookRedirectUri')
+        oauthWorkFlow = OauthWorkFlow(oauthServerName="facebook")
+        client_id, client_secret, scope, redirect_uri = oauthWorkFlow.getRegistryValue()
         code = getattr(self.request, 'code', None)
         facebook = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
         facebook = facebook_compliance_fix(facebook)
@@ -38,19 +59,14 @@ class FacebookLogin(BrowserView):
             authorization_url, state = facebook.authorization_url(self.authorization_base_url)
             self.request.response.redirect(authorization_url)
             return
-        facebook.fetch_token(self.token_url,
-                             client_secret=client_secret,
-                             code=code)
-        getUser = facebook.get('https://graph.facebook.com/me?')
-        user = getUser.json()
+        user = oauthWorkFlow.getUserInfo(facebook, self.token_url, client_secret, code, self.getUrl).json()
 
-        # check has id, if True, login
+        # check has id, if True, is a relogin user, if False, is a new user
         userid = safe_unicode("fb%s") % user["id"]
         if api.user.get(userid=userid) is not None:
             self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
             self.request.RESPONSE.redirect("/")
             return
-
         userInfo = dict(
             fullname=safe_unicode(user.get("name", "")),
             description=safe_unicode(user.get("about", "")),
@@ -58,34 +74,21 @@ class FacebookLogin(BrowserView):
             fbGender=safe_unicode(user.get("gender", "")),
             home_page=safe_unicode(user.get("link", "")),
         )
-        api.user.create(
-            username=safe_unicode("fb%s") % user["id"],
-            email=safe_unicode((user.get("email", ""))),
-            properties=userInfo,
-        )
-# relogin or newuser???
-#        info = ""
-      
-#        for i in user.json().viewitems():
-#            info += "%s : %s\n" % (i[0], i[1])
+        oauthWorkFlow.createUser(userid, safe_unicode((user.get("email", ""))), userInfo)
         self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
         self.request.RESPONSE.redirect("/")
-        self.request.response.redirect("/")
         return
 
 
-
-
 class GoogleLogin(BrowserView):
-    token_url = 'https://accounts.google.com/o/oauth2/token'
-    authorization_base_url = 'https://accounts.google.com/o/oauth2/auth'
+    token_url = "https://accounts.google.com/o/oauth2/token"
+    authorization_base_url = "https://accounts.google.com/o/oauth2/auth"
+    getUrl = "https://www.googleapis.com/oauth2/v1/userinfo"
 
     def __call__(self):
-        registry = getUtility(IRegistry)
-        client_id = registry.get('mingtak.oauthlogin.oauth2login.IOauth2Setting.googleAppId')
-        client_secret = registry.get('mingtak.oauthlogin.oauth2login.IOauth2Setting.googleAppSecret')
-        scope = registry.get('mingtak.oauthlogin.oauth2login.IOauth2Setting.googleScope').split(',')
-        redirect_uri = registry.get('mingtak.oauthlogin.oauth2login.IOauth2Setting.googleRedirectUri')
+        oauthWorkFlow = OauthWorkFlow(oauthServerName="google")
+        client_id, client_secret, scope, redirect_uri = oauthWorkFlow.getRegistryValue()
+        scope = scope.split(',')
         code = getattr(self.request, 'code', None)
         google = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
         if code == None:
@@ -95,46 +98,20 @@ class GoogleLogin(BrowserView):
             authorization_url, state = google.authorization_url(self.authorization_base_url)
             self.request.response.redirect(authorization_url)
             return
-        google.fetch_token(token_url=self.token_url,
-                           client_secret=client_secret,
-                           code=code,)
-        getUser = google.get('https://www.googleapis.com/oauth2/v1/userinfo')
-        user = getUser.json()
-        """
-        userinfo = ''
-        for i in user.viewitems():
-            userinfo += '%s : %s\n' % (i[0], i[1])
-        return userinfo
-        """
-        # check has id, if True, login
+        user = oauthWorkFlow.getUserInfo(google, self.token_url, client_secret, code, self.getUrl).json()
+        # check has id, if True, is a relogin user, if False, is a new user
         userid = safe_unicode("gg%s") % user["id"]
         if api.user.get(userid=userid) is not None:
             self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
             self.request.RESPONSE.redirect("/")
             return
-
         userInfo = dict(
             fullname=safe_unicode(user.get("name", "")),
             location=safe_unicode(user.get("locale", "")),
             fbGender=safe_unicode(user.get("gender", "")),
             home_page=safe_unicode(user.get("link", "")),
         )
-        api.user.create(
-            username=userid,
-            email=safe_unicode((user.get("email", ""))),
-            properties=userInfo,
-        )
-# relogin or newuser???
-#        info = ""
-      
-#        for i in user.json().viewitems():
-#            info += "%s : %s\n" % (i[0], i[1])
+        oauthWorkFlow.createUser(userid, safe_unicode((user.get("email", ""))), userInfo)
         self.context.acl_users.session._setupSession(userid.encode("utf-8"), self.context.REQUEST.RESPONSE)
         self.request.RESPONSE.redirect("/")
-        self.request.response.redirect("/")
         return
-
-
-
-
-
